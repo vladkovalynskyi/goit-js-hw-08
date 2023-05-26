a0fec2c040fcf19f48521310cf7052d69587e98a
Vlad Kovalynskyi <99043529+vladkovalynskyi@users.noreply.github.com>
2023-05-27 00:39:07 +0300

    1 try

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+const VimeoPlayer = require('@vimeo/player');
+const throttle = require('lodash.throttle');
+
+const STORAGE_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const storedTime = localStorage.getItem('STORAGE_TIME_KEY');
+
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
+
+const updateStoredTime = throttle(() => {
+  player.getCurrentTime().then((currentTime) => {
+    localStorage.setItem('STORAGE_TIME_KEY', currentTime);
+  });
+}, 1000);
+
+player.on('timeupdate', updateStoredTime);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const throttle = require('lodash.throttle');
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+const feedbackFormStateKey = 'feedback-form-state';
+
+feedbackForm.addEventListener('input', throttle(saveFormState, 500));
+
+feedbackForm.addEventListener('submit', handleSubmit);
+
+function saveFormState() {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem(feedbackFormStateKey, JSON.stringify(formState));
+}
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formState);
+
+  localStorage.removeItem(feedbackFormStateKey);
+
+  feedbackForm.reset();
+}
